bb900c8670a15ce1785f2d4b7b4d84ea96f5a046
Samoel de Moura <samoelmoura@gmail.com>
2023-10-23 11:18:56 -0300

    Update send_message.py
    hiding keys

--- send_message/send_message.py
+++ send_message/send_message.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import openai
 
 token = 'xxx'
-openai.api_key = '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'
+openai.api_key = 'xxx'
 chat_id = '586530904'
 url = f'https://api.telegram.org/bot6464394089:AAFfCKVcvoEuMYLZuIDeNKpRP_mdiEg0XB8/sendMessage?chat_id={chat_id}'
 
